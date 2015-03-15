10460e33f934b2620e815e561ec1d425410aec80
Pavel Tolstoy <eshikvtumanebest@gmail.com>
2015-03-15 22:17:12 +1000

    Add admin form in vacancies

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from models import Head, Department, Status
 
 # Register your models here.
+class HeadInline(admin.StackedInline):
+    model = Head
+
+class DepartmentAdmin(admin.ModelAdmin):
+    inlines =(
+        HeadInline,
+    )
+    fields = []
+
+class StatusAdmin(admin.ModelAdmin):
+    fields = []
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Status, StatusAdmin)

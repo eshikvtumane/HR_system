53f7f4cf114fa3cc1b123793fff46ee54337508b
Pavel Tolstoy <eshikvtumanebest@gmail.com>
2015-03-16 17:17:05 +1000

    Add applicant view

--- applicants/urls.py
+++ applicants/urls.py
@@ -1,6 +1,6 @@
 #-*- coding:utf-8 -*-
 from django.conf.urls import patterns, include, url
-from views import ApplicantAddView, CandidateSearchView, VacancySearchAjax
+from views import ApplicantAddView, CandidateSearchView, ApplicantView, VacancySearchAjax
 
 
 urlpatterns = patterns('',
@@ -12,9 +12,13 @@ urlpatterns = patterns('',
     url(r'^add/$', ApplicantAddView.as_view(), name='applicant_add'),
     # поиск кандидатов
     url(r'^search/$', CandidateSearchView.as_view(), name='applicant_search'),
+    # страница кандидата
+    url(r'^view/?P<applicant_id>[0-9]+/$', ApplicantView.as_view(), name='applicant_view'),
 
     # выборка вакансий
     url(r'^vacancy_search/$', VacancySearchAjax.as_view(), name='vacancy_search'),
+
+
 
     #url(r'^admin/', include(admin.site.urls)),
 )

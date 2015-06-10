21c723386e92d11a3eb37b350c7550b719d5c26d
inframercury <inframercury@gmail.com>
2015-06-10 17:14:17 +1000

    Add marks for required fields in vacancy app

--- vacancies/static/vacancies/js/vacancy_add.js
+++ vacancies/static/vacancies/js/vacancy_add.js
@@ -1,12 +1,28 @@
 window.onload = function(){
     //валидация формы
 
+     /*$.formUtils.addValidator({
+          name : 'end_date',
+          validatorFunction : function(value, $el, config, language, $form) {
+            if(value.length != 0){
+                var reg = new RegExp('^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$');
+                result = reg.test(value);
+                return result;
+            }
+            return true;
+          },
+          errorMessage : 'В поле с предполагаемым сроком закрытия должно быть указано число!',
+          errorMessageKey: 'badDate'
+        });
+*/
+
+
     $.validate({
 
 
         form:'#frm_add_vacancy',
         onError : function() {
-
+               
     },
         onSuccess:function(){
 
@@ -91,7 +107,7 @@ $('#benefits').selectize({
                 });
                 $heads_div = $('#heads_div');
                 $heads_div.empty();
-                $("<label for='heads'>Руководитель</label>").appendTo($heads_div);
+                $("<label for='heads'>Руководитель:</label>").appendTo($heads_div);
                 $select.appendTo(heads_div)
             },
             error: function(xhr,errmsg,err) {
